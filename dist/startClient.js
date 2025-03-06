0665443895bf94c505a32f98b145a2462e1928b2
Kostya Betenya <betenya.kostya@mail.ru>
2025-03-06 09:19:57 +0300

    fix: build folder add in the repo

--- dist/startClient.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-let client;
-createClient('127.0.0.1', 8800);
-function createClient(ip, port) {
-    client = new server_1.Client(ip, port);
-    client.startServer();
-}
